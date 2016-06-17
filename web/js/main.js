10fb71f805a40041458ba6a82d5bd4464cb10b7e
Amrit Singh Bains <amrit.singh.bains@live.com>
2016-06-17 14:04:27 -0400

    ui upgrade

--- web/js/main.js
+++ web/js/main.js
@@ -15,17 +15,25 @@ function show_more_menu(value) {
 }
 function myFunction() {
     var x = document.getElementById("first_name2");
+    if(x.value.length == 0){
+      return 1;
+    }
     toggle_visibility('loadingMask', 'show');
     $.ajax({url: "https://greaterkwchamber.herokuapp.com/?q=" + x.value, success: function(result){
-        // console.log(result);
+        console.log(result);
         toggle_visibility('loadingMask', 'hide');
         $("#listOfPeople").empty();
         for(var i=0; i<result.length; i++){
           // console.log(result[i])
+          var url = '';
+          if(result[i].ucdirectory_uclisting_hlwebsite.length > 0){
+            url = '<a href="' + result[i].ucdirectory_uclisting_hlwebsite +
+                  '">' + result[i].ucdirectory_uclisting_hlwebsite + '</a>';
+          }
           $("#listOfPeople").append('<li class="collection-item avatar"><i class="material-icons circle">perm_identity</i><span class="title">' + 
-            result[i].ucdirectory_uclisting_lblowner + '</span><p style="text-align:left;">' + 
+            result[i].ucdirectory_uclisting_lblowner + ' ' + url + '</span><p style="text-align:left;">' + 
           result[i].ucdirectory_uclisting_lbladdress1 + ', ' +
-          result[i].ucdirectory_uclisting_lblcity + ', ' +
+          result[i].ucdirectory_uclisting_lblcity +
           result[i].ucdirectory_uclisting_lblstateprovince + '<span style="float:right;">' +
           result[i].ucdirectory_uclisting_lblphone1 + '</span></p></li>');
         }
